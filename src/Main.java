0ab0cea71e1751de0320172f192a1af0637340a5
Administrator <admin@example.com>
2020-08-18 13:28:49 +0300

    Update javafx

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Программа для Кафедры");
+
+        StackPane layout = new StackPane();
+
+
+        Scene scene = new Scene(layout , 300 ,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
