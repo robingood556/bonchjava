45c11eff3ef73f7c13f296991a8656f04e854d67
Administrator <admin@example.com>
2020-08-19 15:00:56 +0300

    Update Button

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,32 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 public class Main extends Application {
+
+    Button button;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Программа для Кафедры");
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Программа для кафедры");
+        button = new Button();
+        button.setText("Выберите файл");
 
         StackPane layout = new StackPane();
+        layout.getChildren().add(button);
 
-
-        Scene scene = new Scene(layout , 300 ,250);
+        Scene scene = new Scene(layout, 300 , 300);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 }
