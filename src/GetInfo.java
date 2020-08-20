2e8d1842c233235279085d3c1f23be2ecf5a3440
ignatrusakov <igat2021@gmail.com>
2020-08-20 15:44:04 +0300

    Work Ignat 20.08

---
+++ src/GetInfo.java
@@ -0,0 +1,13 @@
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+//Класс должен принимать данные из таблицы ecxel
+public class GetInfo {
+    public static void main(String[] args) {
+
+    }
+}
+
+
